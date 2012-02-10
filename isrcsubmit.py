235f0ee12df5d02f7a03fad99377f2bb51345388
Johannes Dewender <github@JonnyJD.net>
2012-02-10 23:18:33 +0100

    add full license text (GPL3), re #8

--- isrcsubmit.py
+++ isrcsubmit.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python2
-# Copyright 2010-2012 Johannes Dewender
+# Copyright (C) 2010-2012 Johannes Dewender
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
