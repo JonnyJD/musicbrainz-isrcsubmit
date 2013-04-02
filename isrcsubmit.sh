a5c57c88e0a991a0906187a6bea6072cbe9d6b67
Johannes Dewender <github@JonnyJD.net>
2013-04-02 13:55:56 +0200

    don't use python2 in isrcsubmit.sh
    The default is now "python" directly in the script
    and we don't care if that is version 2 or 3.
    
    It is completely up to the distribution / packager.
    
    The only thing done in the script is adding "." to PATH
    in order to allow local libdiscid.

--- isrcsubmit.sh
+++ isrcsubmit.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 
-for python in {python2,python2.7,python2.6,python2.5}; do
-	which $python 2>&1 > /dev/null
-	if [ $? == 0 ]; then
-		export PATH=$PATH:.
-		exec $python isrcsubmit.py $@
-	fi
-done
+export PATH=$PATH:.
+exec ./isrcsubmit.py $@
