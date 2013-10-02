1b6c6607e9ac087c130dfb060645a68c50f4c08d
Sebastian Ramacher <sebastian+dev@ramacher.at>
2013-10-02 03:01:03 +0200

    Build a manpage using Sphinx
    Signed-off-by: Sebastian Ramacher <sebastian+dev@ramacher.at>

--- setup.py
+++ setup.py
@@ -1,8 +1,43 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from distutils.command.build import build
 
 from isrcsubmit import __version__
+
+try:
+    import sphinx
+    from sphinx.setup_command import BuildDoc
+    using_sphinx = True
+except ImportError:
+    using_sphinx = False
+
+cmdclass = {}
+if using_sphinx:
+    # use Sphinx to build the manpage if it is available
+    class BuildDocMan(BuildDoc):
+        def initialize_options(self):
+            BuildDoc.initialize_options(self)
+            self.builder = 'man'
+            self.source_dir = 'doc'
+            self.build_dir = 'build'
+
+    # automatically build manpages as sub target of build
+    build.sub_commands.append(('build_sphinx_man', None))
+    cmdclass['build_sphinx_man'] = BuildDocMan
+
+    import platform
+    if platform.system() in ['FreeBSD', 'OpenBSD']:
+        man_dir = 'man'
+    else:
+        man_dir = 'share/man'
+
+    import os.path
+    man_pages = [
+        (os.path.join(man_dir, 'man1'), ['build/man/isrcsubmit.1']),
+    ]
+else:
+    man_pages = []
 
 setup(name="isrcsubmit",
         version=__version__,
@@ -27,7 +62,9 @@ setup(name="isrcsubmit",
             "Topic :: Database :: Front-Ends",
             "Topic :: Multimedia :: Sound/Audio :: CD Audio :: CD Ripping",
             "Topic :: Text Processing :: Filters"
-            ]
+            ],
+        data_files=man_pages,
+        cmdclass=cmdclass
         )
 
 # vim:set shiftwidth=4 smarttab expandtab:
