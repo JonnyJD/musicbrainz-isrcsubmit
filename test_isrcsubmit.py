3e8d23abe82d1f12319db0bf65c83decadeeba0f
Johannes Dewender <github@JonnyJD.net>
2013-10-13 19:30:39 +0200

    test: switch to using TextIOWrapper
    This fixes the problem with sys.stdout.buffer
    and encoding not beeing available.
    This way we are able to catch output for a script run
    including print_encoded() output.
    However, this doesn't work with Python 2 yet.

--- test_isrcsubmit.py
+++ test_isrcsubmit.py
@@ -6,14 +6,19 @@ import os
 import sys
 import math
 import unittest
+from io import TextIOWrapper, BytesIO
 
+import discid
+import musicbrainzngs
 import isrcsubmit
 
+
 try:
-    from cStringIO import StringIO      # Python 2
+    import discid
+    from discid import DiscError
 except ImportError:
-    from io import StringIO             # Python 3
-    # io is already present in Python 2.6, but works with unicode instead of str
+    from libdiscid.compat import discid
+    from libdiscid.compat.discid import DiscError
 
 SCRIPT_NAME = "isrcsubmit.py"
 
@@ -67,15 +72,88 @@ class TestInternal(unittest.TestCase):
         os.dup2(self._old_stdout, 1)
 
 
+TEST_DATA = "test_data/"
+save_run = True
+data_sent = {}
+mocked_disc = None
+
+class Mocked_Disc(object):
+    def __init__(self, id, mcn, tracks):
+        self.id = id
+        self.mcn = mcn
+        self.tracks = tracks
+
+
+# mock musicbrainzngs queries
+# - - - - - - - - - - - - - -
+
+# save mbngs functions
+_mbngs_get_releases_by_discid = musicbrainzngs.get_releases_by_discid
+_mbngs_get_release_by_id = musicbrainzngs.get_release_by_id
+_mbngs_submit_isrcs = musicbrainzngs.submit_isrcs
+
+def _get_releases_by_discid(disc_id, includes=[]):
+    file_name = "%s%s_releases.py" % (TEST_DATA, disc_id)
+    if save_run:
+        releases = _mbngs_get_releases_by_discid(disc_id, includes)
+        with open(file_name, "w") as releases_file:
+            releases_file.write(repr(releases))
+        return releases
+    else:
+        with open(file_name, "r") as releases_file:
+            return releases_file.read()
+
+musicbrainzngs.get_releases_by_discid = _get_releases_by_discid
+
+def _get_release_by_id(release_id, includes=[]):
+    file_name = "%s%s.py" % (TEST_DATA, release_id)
+    if save_run:
+        release = _mbngs_get_release_by_id(release_id, includes)
+        with open(file_name, "w") as release_file:
+            releases_file.write(repr(release))
+        return releases
+    else:
+        with open(file_name, "r") as release_file:
+            return release_file.read()
+
+musicbrainzngs.get_release_by_id = _get_release_by_id
+
+def _submit_isrcs(tracks2isrcs):
+    global data_sent
+    data_sent.tracks2isrcs = tracks2isrcs
+    return True
+
+musicbrainzngs.submit_isrcs = _submit_isrcs
+
+
+# mock discid reading
+# - - - - - - - - - -
+
+# save discid functions
+_discid_read = discid.read
+
+def _read(device=None, features=[]):
+    if save_run:
+        return _discid_read(device, features)
+    else:
+        return mocked_disc
+
+discid.read = _read
+
+
 class TestScript(unittest.TestCase):
     def setUp(self):
         # gather output
         self._old_stdout = sys.stdout
-        self._stdout = StringIO()
+        self._stdout = TextIOWrapper(BytesIO(), sys.stdout.encoding)
         sys.stdout = self._stdout
 
     def _output(self):
-        return self._stdout.getvalue()
+        self._stdout.seek(0)
+        return self._stdout.read()
+
+    def _debug(self):
+        return sys.stderr.write(self._output())
 
     def test_version(self):
         try:
@@ -92,6 +170,16 @@ class TestScript(unittest.TestCase):
             pass
         finally:
             self.assertTrue(self._output().strip())
+
+    def test_read(self):
+        global mocked_disc
+        mocked_disc = Mocked_Disc("id zeug", "064811650", [])
+        try:
+            isrcsubmit.main([SCRIPT_NAME])
+        except SystemExit:
+            pass
+        finally:
+            self.assertTrue(isrcsubmit.__version__ in self._output().strip())
 
     def tearDown(self):
         # restore output
